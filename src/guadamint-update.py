ac84e6f9f8118aa68517a9c3ad48bb6f1238491a
aosucas499 <aosucas499@gmail.com>
2026-01-29 23:01:59 +0100

    Update multimedia section in guadamint-update.py
    Removed 'childsplay' and 'pinta' from multimedia section.

--- src/guadamint-update.py
+++ src/guadamint-update.py
@@ -33,7 +33,7 @@ APPS_OBLIGATORIAS = [
     "scratch", "kturtle", "thonny", "minetest",
     
     # Multimedia y Creatividad
-    "gcompris-qt", "childsplay", "audacity", "pinta",
+    "gcompris-qt", "audacity",
     
     # Mecanografía formal
     "klavaro"
