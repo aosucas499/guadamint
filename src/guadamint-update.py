800147c9487aedbe7cde625eb72806488f50d5ab
aosucas499 <aosucas499@gmail.com>
2026-02-10 00:07:35 +0100

    Fix formatting issues in guadamint-update.py

--- src/guadamint-update.py
+++ src/guadamint-update.py
@@ -31,7 +31,7 @@ APPS_OBLIGATORIAS = [
     "kgeography", "kwordquiz", "klettres", "khangman", "kanagram", 
     "stellarium", "kalzium", "step", "gbrainy", "marble",
     "scratch", "kturtle", "thonny", "minetest",
-    "gcompris-qt", "audacity", "openboard", "klavaro"
+    "gcompris-qt", "audacity", "openboard", 
 ]
 
 logging.basicConfig(filename=LOG_FILE, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
