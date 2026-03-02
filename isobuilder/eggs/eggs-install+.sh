a504a4e34a408f3098c9ae2049ddebe12cf55a66
aosucas499 <aosucas499@gmail.com>
2026-03-02 01:44:57 +0100

    Add files via upload

--- isobuilder/eggs/eggs-install+.sh
+++ isobuilder/eggs/eggs-install+.sh
@@ -6,13 +6,16 @@
 #sudo apt-get install -f -y
 
 #copiar theme educaandos para la ISO
-sudo cp -r /home/$USER/guadalinex/isobuilder/eggs/guadamint /usr/lib/penguins-eggs/addons/
+#sudo cp -r /home/$USER/guadamint/isobuilder/eggs/guadamint /usr/lib/penguins-eggs/addons/
 
 # Modificar los grupos que instalará calamares al nuevo usuario (no necesario versión 9.3.8 eggs)
 #sudo cp /home/$USER/guadalinex/isobuilder/eggs/educaandos/theme/calamares/modules/users.yml /usr/lib/penguins-eggs/conf/distros/focal/calamares/modules/users.yml
 
 #instalar calamares (and configure it to act without root)
-sudo eggs calamares --install --theme guadamint
+sudo eggs calamares --install
+#sudo eggs calamares --install --theme guadamint
+sudo cp -r /home/$USER/guadamint/isobuilder/eggs/guadamint /etc/calamares/branding
+
 
 # paquetes necesarios para instalación en EFI secureboot
 #sudo dpkg -i grub-efi-amd64-signed*amd64.deb
@@ -30,7 +33,7 @@ sudo eggs dad -d
 
 # Modify settings for iso
 #sudo nano /etc/penguins-eggs.d/eggs.yaml
-sudo cp /home/$USER/guadalinex/isobuilder/eggs/eggs.yaml /etc/penguins-eggs.d/
+sudo cp /home/$USER/guadamint/isobuilder/eggs/eggs.yaml /etc/penguins-eggs.d/
 
 # Tenemos que modificar el archivo eggs.yaml a mano e incluir el vmlinuz y initrd con la versión, no dejar sin la versión.
 # así como los lenguajes, Europe/Madrid para la hora. Usar # sudo nano /etc/penguins-eggs.d/eggs.yaml después de estos dos comandos.
